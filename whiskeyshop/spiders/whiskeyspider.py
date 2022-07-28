0dea14f49764eea6d2db1dd6c1b4b60dc8dfe39f
Onkar Patil <102043839+Foxhog-x@users.noreply.github.com>
2022-07-28 20:12:08 +0530

    Add files via upload

---
+++ whiskeyshop/spiders/whiskeyspider.py
@@ -0,0 +1,24 @@
+import scrapy
+
+class whiskeyspider(scrapy.Spider):
+        
+    name = 'whiskey'
+    start_urls = ['https://www.whiskyshop.com/scotch-whisky/all?item_availability=In+Stock']
+     
+    def parse(self, response):
+            for product in response.css('div.product-item-info'):
+                    try:
+                        yield {
+                            'name': product.css('a.product-item-link::text').get(),
+                            'price': product.css('span.price::text').get().replace('£', '').strip(),
+                            'link': product.css('a.product-item-link').attrib['href'],
+                        }
+                    except:
+                        yield {
+                            'name': product.css('a.product-item-link::text').get(),
+                            'price': 'sold out',
+                            'link': product.css('a.product-item-link').attrib['href'],
+                        }
+            next_page = response.css('a.action.next').attrib['href']
+            if next_page is not None:
+                yield response.follow(next_page, callback=self.parse)
